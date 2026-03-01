dbc8dc3ea13353f151921756a3a0ae70b8bd7f7a
Pooja-Basavaraj17 <poojusmith17@gmail.com>
2026-03-01 21:11:41 +0530

    Support both local and Railway DB

--- db.js
+++ db.js
@@ -6,10 +6,11 @@ const mysql = require("mysql2");
 
 // ================= CREATE CONNECTION POOL =================
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || process.env.MYSQLHOST,
+  user: process.env.DB_USER || process.env.MYSQLUSER,
+  password: process.env.DB_PASS || process.env.MYSQLPASSWORD,
+  database: process.env.DB_NAME || process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306,	
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
